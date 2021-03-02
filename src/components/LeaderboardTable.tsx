b042bef9bb370d571476a3a4e3b6f078f603f218
Rellyson <rellysondouglas2015@gmail.com>
2021-03-02 09:14:20 -0300

    added leadeboard card table

---
+++ src/components/LeaderboardTable.tsx
@@ -0,0 +1,22 @@
+import styles from '../styles/components/LeaderboardTable.module.css'
+
+export function LeaderboardTable() {
+    return (
+        <table>
+            <thead>
+                <th>Posição</th>
+                <th>Usuário</th>
+                <th>Desafios</th>
+                <th>Experiência</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>01</td>
+                    <td>Diego Fernandes</td>
+                    <td>01</td>
+                    <td>01</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
