19f8aa2c4a2fa1b5146936b35489f90c0a435b4a
Rellyson <rellysondouglas2015@gmail.com>
2021-03-03 13:41:27 -0300

    Getting leadboard page and its styles

--- src/components/LeaderboardTable.tsx
+++ src/components/LeaderboardTable.tsx
@@ -1,22 +1,56 @@
 import styles from '../styles/components/LeaderboardTable.module.css'
+import { Card } from './Card'
 
 export function LeaderboardTable() {
     return (
-        <table>
-            <thead>
-                <th>Posição</th>
-                <th>Usuário</th>
-                <th>Desafios</th>
-                <th>Experiência</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>01</td>
-                    <td>Diego Fernandes</td>
-                    <td>01</td>
-                    <td>01</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className={styles.leaderboardContainer} >
+            <div className={styles.cards} >
+                <div className={styles.head}>
+                    <span>Posição</span>
+                    <span>Usuário</span>
+                    <span>Desafios</span>
+                    <span>Experiência</span>
+                </div>
+
+                <Card
+                    position={1}
+                    user={{
+                        avatar: "https://github.com/diego3g.png",
+                        name: "Diego Fernandes",
+                        level: 43,
+                        completedChallenges: 127,
+                        totalExperience: 154000,
+                    }}
+                />
+
+                <Card
+                    position={2}
+                    user={{
+                        avatar: "https://github.com/rellyso.png",
+                        name: "Rellyson",
+                        level: 20,
+                        completedChallenges: 50,
+                        totalExperience: 54000,
+                    }}
+                />
+
+            </div>
+        </div>
+        // <table>
+        //     <thead>
+        //         <th>Posição</th>
+        //         <th>Usuário</th>
+        //         <th>Desafios</th>
+        //         <th>Experiência</th>
+        //     </thead>
+        //     <tbody>
+        //         <tr>
+        //             <td>01</td>
+        //             <td>Diego Fernandes</td>
+        //             <td>01</td>
+        //             <td>01</td>
+        //         </tr>
+        //     </tbody>
+        // </table>
     )
 }
